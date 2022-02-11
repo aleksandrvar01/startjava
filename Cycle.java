cac2733dba935a324d587a031d8980fd72d458f4
aleksandrvar01 <98684730+aleksandrvar01@users.noreply.github.com>
2022-02-12 01:17:56 +0300

    1
    1

--- Cycle.java
+++ Cycle.java
@@ -3,5 +3,14 @@ public class Cycle {
     for (int i = 0; i <= 20; i++) {
         System.out.print(i + " ");
     }
+
+    System.out.println("\n");
+    
+    int num = 6;
+    while( num >= -6) {
+        System.out.print(num + " ");
+        num = num - 2;
+    }
+
     }
 }
