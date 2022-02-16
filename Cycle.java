b4bb7161d569935149eadf721baece80e5853db7
aleksandrvar01 <98684730+aleksandrvar01@users.noreply.github.com>
2022-02-17 00:31:12 +0300

    1
    1

--- Cycle.java
+++ Cycle.java
@@ -2,14 +2,14 @@ public class Cycle {
     public static void main(String[] args) {
         //Выведите на консоль с помощью цикла for все числа от [0, 20]
         System.out.println("Выведите на консоль с помощью цикла for все числа от [0, 20]");  
-        for(int i = 0; i <= 20; i++) {
+        for (int i = 0; i <= 20; i++) {
             System.out.print(i + " ");
         }
 
         //Выведите на консоль с помощью цикла while все числа от [6, -6] (с шагом итерации 2)
         System.out.println("\nВыведите на консоль с помощью цикла while все числа от [6, -6] (с шагом итерации 2)");
         int num = 6;
-        while(num >= -6) {
+        while (num >= -6) {
             System.out.print(num + " ");
             num -= 2;
         }
@@ -21,7 +21,7 @@ public class Cycle {
         do {
             a++;
             sum = a % 2 != 0 ? sum + a : sum;
-        } while(a < 20);
+        } while (a < 20);
         System.out.println(sum);
     }
 }
