2b1ca98a17090e595fca911622f2f8d1d0be8619
aleksandrvar01 <98684730+aleksandrvar01@users.noreply.github.com>
2022-02-16 23:31:32 +0300

    1
    1

---
+++ MyFirstGame.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int a = 38;
+        Scanner input = new Scanner(System.in);
+        int number;
+        do {
+            System.out.println("Угадай число от 0 до 100");
+            number = input.nextInt();
+            if (a > number) {
+                System.out.println("Данное число меньше того, что загадал компьютер");
+            } else if (a < number) {
+                System.out.println("Данное число больше того, что загадал компьютер");
+            }
+        } while (a != number);
+        System.out.println("Поздравляю, число угадано!");
+    }
+}
