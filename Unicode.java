591dddaa089f5bbb854b6ec0b202221951bee0d1
aleksandrvar01 <98684730+aleksandrvar01@users.noreply.github.com>
2022-02-17 23:06:43 +0300

    Update Unicode.java

--- Unicode.java
+++ Unicode.java
@@ -1,10 +1,7 @@
 public class Unicode {
     public static void main(String[] args) {
-        int number1 = 33;
-        int number2 = 126;
-        for (int i = number1; i <= number2; i++) {
-            System.out.print((char) number1 + " ");
-            number1 += 1;
+        for (char i = 33; i <= 126; i++) {
+            System.out.print(i + " ");
         }
     }
 }
