271e1782d9005b32eb4e397124ab51c28b1afa74
aleksandrvar01 <98684730+aleksandrvar01@users.noreply.github.com>
2022-02-17 15:55:00 +0300

    1

--- Unicode.java
+++ Unicode.java
@@ -4,8 +4,8 @@ public class Unicode {
         int number1 = 33;
         int number2 = 126;
         do {
-            System.out.print(symbol = (char) number);
-            number += 1;
+            System.out.print((symbol = (char) number1) + " ");
+            number1 += 1;
         } while (number1 <= number2);   
     }
 }
