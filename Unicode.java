cb926fd72de2d51bbd881cbd91ac7a1a3c42c8f4
aleksandrvar01 <98684730+aleksandrvar01@users.noreply.github.com>
2022-02-17 15:36:15 +0300

    Unicode.java
    1

--- Unicode.java
+++ Unicode.java
@@ -1,11 +1,11 @@
 public class Unicode {
     public static void main(String[] args) {
         char symbol;
-        int number = 9398;
-        int number2 = 10178;
-        for (int i = 0; i < 7; i++) {
-            int number3 = number + (int) (Math.random() * number2);
-            System.out.println(symbol = (char) number3);
-        }
+        int number1 = 33;
+        int number2 = 126;
+        do {
+            System.out.print(symbol = (char) number);
+            number += 1;
+        } while (number1 <= number2);   
     }
 }
