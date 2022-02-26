16851e50417287a823fe5440bac53d04b83e1541
aleksandrvar01 <98684730+aleksandrvar01@users.noreply.github.com>
2022-02-27 00:06:30 +0300

    1

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -1,3 +1,24 @@
 public class Person {
 
+	String gender = "male";
+	Srting name = "Alex";
+	float height = 175.5f;
+	float weight = 80.5f;
+	int age = 35;
+
+	void talk() {
+		System.out.println("Говорит");
+	}
+
+	void run() {
+		System.out.println("Бежит");
+	}
+
+	void learnJava() {
+		System.out.println("Учит Java");
+	}
+
+	void think() {
+		System.out.println("Думает");
+	}
 }
