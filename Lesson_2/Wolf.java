82c98c58d2822061177abb7117b605d849607996
aleksandrvar01 <98684730+aleksandrvar01@users.noreply.github.com>
2022-02-27 01:17:58 +0300

    1

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,27 +1,28 @@
 public class Wolf {
 
-	String gender;
-	String name;
-	float weight;
-	int age;
-	String color;
+    String gender;
+    String name;
+    float weight;
+    int age;
+    String color;
 
-	void howls() {
-		System.out.println("Воет");
-	}
+    void howls() {
+        System.out.println("Воет");
+    }
 
-	void sit() {
-		System.out.println("Сидит");
-	}
+    void sit() {
+        System.out.println("Сидит");
+    }
 
-	void run() {
-		System.out.println("Бежит");
-	}
-	void go() {
-		System.out.println("Идёт");
-	}
+    void run() {
+        System.out.println("Бежит");
+    }
 
-	void hunt() {
-		System.out.println("Охотится");
-	}
+    void go() {
+        System.out.println("Идёт");
+    }
+
+    void hunt() {
+        System.out.println("Охотится");
+    }
 }
