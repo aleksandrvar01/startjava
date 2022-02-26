9ae0c1de6004ba7987369dff2898b9ce66204470
aleksandrvar01 <98684730+aleksandrvar01@users.noreply.github.com>
2022-02-27 00:14:44 +0300

    1

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -1,24 +1,24 @@
 public class Person {
 
-	String gender = "male";
-	String name = "Alex";
-	float height = 175.5f;
-	float weight = 80.5f;
-	int age = 35;
+    String gender = "male";
+    String name = "Alex";
+    float height = 175.5f;
+    float weight = 80.5f;
+    int age = 35;
 
-	void talk() {
-		System.out.println("Говорит");
-	}
+    void talk() {
+        System.out.println("Говорит");
+    }
 
-	void run() {
-		System.out.println("Бежит");
-	}
+    void run() {
+        System.out.println("Бежит");
+    }
 
-	void learnJava() {
-		System.out.println("Учит Java");
-	}
+    void learnJava() {
+        System.out.println("Учит Java");
+    }
 
-	void think() {
-		System.out.println("Думает");
-	}
+    void think() {
+        System.out.println("Думает");
+    }
 }
